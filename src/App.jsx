e2a743f0742eea433632777e699455cc1a3aae08
BobbyWhitacre27 <bobby.whitacre27@gmail.com>
2023-04-14 22:54:59 -0400

    Mobile view fixed

--- src/App.jsx
+++ src/App.jsx
@@ -97,7 +97,7 @@ function App() {
                 <input type="radio" value="13" name="options" data-title="13" class="btn w-fit" />
                 <input type="radio" value="14" name="options" data-title="14" class="btn w-fit" />
                 <input type="radio" value="15" name="options" data-title="15" class="btn w-fit" />
-                <input type="radio" value="other" name="options" data-title="Other" class="btn" onClick={() => setIsLeaseTermOther(true)} />
+                <input type="radio" value="other" name="options" data-title="Other" class="btn w-fit" onClick={() => setIsLeaseTermOther(true)} />
               </div>
               {isLeaseTermOther === true ? <div><input type="text" onChange={handleLeaseTermOther} placeholder="Lease Term" class="input input-bordered w-full max-w-xs mt-8" /></div> : ""}
 
@@ -110,7 +110,7 @@ function App() {
             <form>
               <div class="btn-group w-fit" onChange={handleSpecial}>
                 <input type="radio" value="1" name="options" data-title="1 Month Free" class="btn text-xs w-fit" />
-                <input type="radio" value="2" name="options" data-title="2 Months Free" class="btn text-x w-fit" />
+                <input type="radio" value="2" name="options" data-title="2 Months Free" class="btn text-xs w-fit" />
                 <input type="radio" value="other" name="options" data-title="Other" class="btn w-fit" onClick={() => setIsSpecialOther(true)} />
               </div>
               {isSpecialOther === true ? <div><input type="text" onChange={handleSpecialOther} placeholder="Enter special in $" class="input input-bordered w-full max-w-xs mt-8" /></div> : ""}
