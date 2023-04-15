9408da6ad35cf36b3d2a51b9a7576762d1718ea5
BobbyWhitacre27 <bobby.whitacre27@gmail.com>
2023-04-14 22:53:28 -0400

    CSS updates

--- src/App.jsx
+++ src/App.jsx
@@ -93,10 +93,10 @@ function App() {
               Lease Term
             </p>
             <form>
-              <div class="btn-group" onChange={handleLeaseTerm}>
-                <input type="radio" value="13" name="options" data-title="13" class="btn" />
-                <input type="radio" value="14" name="options" data-title="14" class="btn" />
-                <input type="radio" value="15" name="options" data-title="15" class="btn" />
+              <div class="btn-group w-fit" onChange={handleLeaseTerm}>
+                <input type="radio" value="13" name="options" data-title="13" class="btn w-fit" />
+                <input type="radio" value="14" name="options" data-title="14" class="btn w-fit" />
+                <input type="radio" value="15" name="options" data-title="15" class="btn w-fit" />
                 <input type="radio" value="other" name="options" data-title="Other" class="btn" onClick={() => setIsLeaseTermOther(true)} />
               </div>
               {isLeaseTermOther === true ? <div><input type="text" onChange={handleLeaseTermOther} placeholder="Lease Term" class="input input-bordered w-full max-w-xs mt-8" /></div> : ""}
@@ -108,10 +108,10 @@ function App() {
             </p>
 
             <form>
-              <div class="btn-group" onChange={handleSpecial}>
-                <input type="radio" value="1" name="options" data-title="1 Month Free" class="btn text-xs" />
-                <input type="radio" value="2" name="options" data-title="2 Months Free" class="btn text-xs" />
-                <input type="radio" value="other" name="options" data-title="Other" class="btn" onClick={() => setIsSpecialOther(true)} />
+              <div class="btn-group w-fit" onChange={handleSpecial}>
+                <input type="radio" value="1" name="options" data-title="1 Month Free" class="btn text-xs w-fit" />
+                <input type="radio" value="2" name="options" data-title="2 Months Free" class="btn text-x w-fit" />
+                <input type="radio" value="other" name="options" data-title="Other" class="btn w-fit" onClick={() => setIsSpecialOther(true)} />
               </div>
               {isSpecialOther === true ? <div><input type="text" onChange={handleSpecialOther} placeholder="Enter special in $" class="input input-bordered w-full max-w-xs mt-8" /></div> : ""}
 
