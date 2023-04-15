05c6e69454a02b9a6b97a2e489eeb5e471213612
BobbyWhitacre27 <bobby.whitacre27@gmail.com>
2023-04-14 22:19:56 -0400

    100% working

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,18 @@ import './App.css';
 function App() {
   const [rentInput, setRentInput] = useState(0)
   const [leaseTerm, setLeaseTerm] = useState(0)
-  const [special, setSpecial] = useState(0)
+  const [specialSelection, setSpecialSelection] = useState(0)
+  const [specialAmount, setSpecialAmount] = useState(0)
+  const [otherSpecial, setOtherSpecial] = useState(0)
   const [result, setResult] = useState(0)
 
   const [isLeaseTermOther, setIsLeaseTermOther] = useState(false)
-  
-
+  const [isSpecialOther, setIsSpecialOther] = useState(false)
 
   const totalRentCost = rentInput * leaseTerm
-  const answer = (totalRentCost - special) / leaseTerm
+
+  const answer = (totalRentCost - specialAmount) / leaseTerm
+
   const answerCurrency = answer.toLocaleString('en-US', {
     style: 'currency',
     currency: 'USD',
@@ -26,43 +29,63 @@ function App() {
 
   const handleLeaseTerm = event => {
     setLeaseTerm(event.target.value)
+    if (isLeaseTermOther === true) {
+      setIsLeaseTermOther(false)
+    }
   }
+
   const handleLeaseTermOther = event => {
     setLeaseTerm(event.target.value)
   }
 
   const handleSpecial = event => {
-    setSpecial(event.target.value)
+    setSpecialSelection(event.target.value)
+    if (isSpecialOther === true) {
+      setIsSpecialOther(false)
+    }
   }
 
   const handleSpecialOther = event => {
-    setSpecial(event.target.value)
+    setOtherSpecial(event.target.value)
   }
 
   const handleResult = event => {
+    setSpecialSelection(specialSelection)
     setResult(answerCurrency)
   }
 
   useEffect(() => {
+    const specialCalculation = () => {
+      if (specialSelection === "other") {
+        setSpecialAmount(otherSpecial)
+      } else if (specialSelection === "1") {
+        const amount = rentInput * 1
+        setSpecialAmount(amount)
+        setOtherSpecial(0)
+      } else if (specialSelection === "2") {
+        const amount = rentInput * 2
+        setSpecialAmount(amount)
+        setOtherSpecial(0)
+      }
+    }
+    specialCalculation()
+  }, [rentInput, specialAmount, specialSelection, otherSpecial])
 
-  }, [])
 
   return (
     <div className="App">
 
 
-      <section class="bg-gray-50">
-        <div
-          class="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center"
+      <section class="bg-[url(https://images.pexels.com/photos/439391/pexels-photo-439391.jpeg?auto=compress&cs=tinysrgb&w=800)] bg-no-repeat bg-cover">
+        <section><div
+          class="mx-auto max-w-screen-xl px-4 py-8 lg:flex lg:h-screen lg:items-center "
         >
-          <div class="mx-auto max-w-xl text-center">
-            <h1 class="text-3xl font-extrabold sm:text-5xl">
+          <div class="bg-gray-50 bg-opacity-80 border-none mx-auto max-w-xl text-center border rounded p-6 ">
+            <h1 class="text-3xl font-extrabold sm:text-5xl opacity-100">
               Rent Specials Calculator
             </h1>
 
-            <h1 class="text-2xl mt-6">Avg. {result} /month</h1>
-            {/* <h1>Lease Term {leaseTerm}</h1>
-            <h1>Special {special}</h1> */}
+            <h1 class="text-2xl mt-6 opacity-110">Avg. {result}/month</h1>
 
             <input type="text" onChange={handleRentInput} placeholder="Monthly Rent" class="input input-bordered w-full max-w-xs mt-8" />
 
@@ -70,13 +93,13 @@ function App() {
               Lease Term
             </p>
             <form>
-            <div class="btn-group" onChange={handleLeaseTerm}>
-              <input type="radio" value="13" name="options" data-title="13" class="btn" />
-              <input type="radio" value="14" name="options" data-title="14" class="btn" />
-              <input type="radio" value="15" name="options" data-title="15" class="btn" />
-              <input type="radio" value="other" name="options" data-title="Other" class="btn" />
-            </div>
-            {leaseTerm === "other" ? <div><input type="text" onChange={handleLeaseTermOther} placeholder="Lease Term" class="input input-bordered w-full max-w-xs mt-8" /></div> : ""}
+              <div class="btn-group" onChange={handleLeaseTerm}>
+                <input type="radio" value="13" name="options" data-title="13" class="btn" />
+                <input type="radio" value="14" name="options" data-title="14" class="btn" />
+                <input type="radio" value="15" name="options" data-title="15" class="btn" />
+                <input type="radio" value="other" name="options" data-title="Other" class="btn" onClick={() => setIsLeaseTermOther(true)} />
+              </div>
+              {isLeaseTermOther === true ? <div><input type="text" onChange={handleLeaseTermOther} placeholder="Lease Term" class="input input-bordered w-full max-w-xs mt-8" /></div> : ""}
 
             </form>
 
@@ -85,26 +108,25 @@ function App() {
             </p>
 
             <form>
-            <div class="btn-group" onChange={handleSpecial}>
-              <input type="radio" value={rentInput} name="options" data-title="1 Month Free" class="btn" />
-              <input type="radio" value={rentInput * 2} name="options" data-title="2 Months Free" class="btn" />
-              <input type="radio" value="other" name="options" data-title="Other" class="btn" />
-            </div>
-            {special === "other" ? <div><input type="text" onChange={handleSpecialOther} placeholder="Enter special recieved in dollars" class="input input-bordered w-full max-w-xs mt-8" /></div> : ""}
+              <div class="btn-group" onChange={handleSpecial}>
+                <input type="radio" value="1" name="options" data-title="1 Month Free" class="btn text-xs" />
+                <input type="radio" value="2" name="options" data-title="2 Months Free" class="btn text-xs" />
+                <input type="radio" value="other" name="options" data-title="Other" class="btn" onClick={() => setIsSpecialOther(true)} />
+              </div>
+              {isSpecialOther === true ? <div><input type="text" onChange={handleSpecialOther} placeholder="Enter special in $" class="input input-bordered w-full max-w-xs mt-8" /></div> : ""}
 
             </form>
 
-            <div class="mt-8">
-              <button onClick={handleResult} class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg bg-red-600">Calculate</button>
+            <div class="mt-8 opacity-100">
+              <button onClick={handleResult} class="bg-red-500 hover:bg-red-700 text-white font-bold py-3 px-6 rounded mb-4 text-2xl">
+                Calculate
+              </button>
             </div>
 
           </div>
         </div>
+        </section>
       </section>
-
-
-
-
 
 
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./public/index.html", './src/App.jsx'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'apartment-building': "url('https://images.pexels.com/photos/2111768/pexels-photo-2111768.jpeg?auto=compress&cs=tinysrgb&w=800')"
+      }
+    },
   },
   plugins: [require("@tailwindcss/forms")({
     strategy: 'class',
